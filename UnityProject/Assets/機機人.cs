ec90f23c7da05a8bba08dea005e0b77c003e8180
yf7890yf7890 <55218718+yf7890yf7890@users.noreply.github.com>
2019-11-07 11:37:30 +0800

    期中

---
+++ UnityProject/Assets/機機人.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class 機機人 : MonoBehaviour {
+
+    [Header("對話")]
+    public string NPC;
+    public float sayspeed;
+    [Header("任務")]
+    public bool complete;
+    public int BateryFinish;
+    public int Batery;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+
+}
+
---
+++ UnityProject/Assets/機機球.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+public class 機機球 : MonoBehaviour {
+
+    Rigidbody GG;
+    float gogo;
+    float inin;
+    [Header("速度")]
+    [Range(0,7)]
+    public float speed ;
+    [Header("跳躍")]
+    [Range(0,7)]
+    public float jumpfloat;
+    Transform left;
+    public bool isground;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        GG = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        /// <summary>
+        /// 跑跑
+        /// </summary>
+        gogo = Input.GetAxis("Horizontal");
+        inin = Input.GetAxis("Vertical");
+        GG.AddForce(new Vector3(gogo * speed, 0));
+        GG.AddForce(new Vector3(0,0, inin * speed));
+
+        /// <summary>
+        /// 跳跳
+        /// </summary>
+
+        if (Input.GetKeyDown(KeyCode.Space) && isground == true)
+
+        {
+            isground = false;
+            jump();
+        }
+    }
+
+    /// <summary>
+    /// 判斷
+    /// </summary>
+
+    private void jump()
+    {
+        GG.AddForce(transform.up*100);
+    }
+
+    public void OnCollisionStay(Collision collision)
+    {
+        isground = true;
+    }
+}
